29d721323f7598c506952e4675daffe8e57c7b55
stuartelimu <stuartelimu@gmail.com>
2021-07-19 14:06:51 +0300

    add postgres service to ci workflow

--- movies/settings/gh-actions.py
+++ movies/settings/gh-actions.py
@@ -19,3 +19,15 @@ DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'github_actions',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
